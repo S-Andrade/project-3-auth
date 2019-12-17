7df3d16d95dd74e2f35a4418dd15e7c4a6f8b54b
sandra <sandraandrade@ua.pt>
2019-12-17 06:26:45 +0000

    aaaaaa

---
+++ server_auth.py
@@ -0,0 +1,44 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes
+import datetime
+import PyKCS11
+
+
+#criar certificado do servidor
+key = rsa.generate_private_key(public_exponent=65537,key_size=2048,backend=default_backend())
+
+subject = issuer = x509.Name([
+    x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
+    x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"California"),
+    x509.NameAttribute(NameOID.LOCALITY_NAME, u"San Francisco"),
+    x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"My Company"),
+    x509.NameAttribute(NameOID.COMMON_NAME, u"mysite.com")
+    ])
+
+cert = x509.CertificateBuilder().subject_name(
+        subject
+        ).issuer_name(
+                issuer
+                ).public_key(
+                        key.public_key()
+                        ).serial_number(x509.random_serial_number()
+                                ).not_valid_before(
+                                        datetime.datetime.utcnow()
+                                        ).not_valid_after(
+                                                datetime.datetime.utcnow() + datetime.timedelta(days=10)
+                                                ).add_extension(
+                                                        x509.SubjectAlternativeName([x509.DNSName(u"localhost")]),critical=False,
+                                                        ).sign(key, hashes.SHA256(), default_backend())
+
+
+
+print(cert)
+
+
+
+#assinar um texto com o certificado do servidor
+
