59943f429c27d20de4418bce4ce5f5593f1362e2
sandra <sandraandrade@ua.pt>
2019-12-17 07:06:18 +0000

    yupy

--- cartao.py
+++ cartao.py
@@ -4,12 +4,15 @@ import PyKCS11
 import binascii
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
 lib = '/usr/local/lib/libpteidpkcs11.so'
 pkcs11 = PyKCS11.PyKCS11Lib()
 pkcs11.load(lib)
 slots = pkcs11.getSlotList()
 
+certificate = None
 for slot in slots:
     #print(pkcs11.getTokenInfo(slot))
     all_attr = list(PyKCS11.CKA.keys())
@@ -26,20 +29,20 @@ for slot in slots:
         if str(attr['CKA_LABEL']) == "b'CITIZEN AUTHENTICATION CERTIFICATE'":
             try:
                 certificate = x509.load_der_x509_certificate(bytes(attr['CKA_VALUE']),default_backend())
-                print("certtificado")
-                print(certificate)
             except:
                 print("Something else went wrong")
 
     private_key = session.findObjects([(PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY),(PyKCS11.CKA_LABEL,'CITIZEN AUTHENTICATION KEY')])[0]
-    #print("private key")
-    #print(private_key)
     mechanism = PyKCS11.Mechanism(PyKCS11.CKM_SHA1_RSA_PKCS, None)
     text = b'text to sign'
     signature = bytes(session.sign(private_key, text, mechanism))
-    print("signature")
-    print(signature)
-    print("wtf")
-    print(bytes(binascii.hexlify(signature)))
+    
+
+
+
+print(signature)
+print(text)
+public_key = certificate.public_key()
+public_key.verify(signature,text,padding.PKCS1v15(),hashes.SHA1())
 
 
